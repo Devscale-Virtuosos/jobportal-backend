b9270fbf8a4160c431f55b0f4cecbdda0f8caba7
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2024-10-12 10:14:22 +0700

    setup cors to allow all request

--- src/main.ts
+++ src/main.ts
@@ -18,12 +18,7 @@ connectMongodb();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: env.CLIENT_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
